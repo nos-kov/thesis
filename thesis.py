61b16c60ba2561a78b5638548129c5ca4f11e339
Dmitry Noskov <dvnoskov88@gmail.com>
2023-06-14 21:37:33 +0200

    fixed

--- thesis.py
+++ thesis.py
@@ -3,11 +3,12 @@ import requests
 from pprint import pprint
 
 
+def get_user_data(token, user_id, ya_token):
 
-def get_user_data(token):
-
-    url = 'https://api.vk.com/method/users.get'
-    params = {'user_ids': '1', 'access_token': token, 'v': '5.131'}
+    #url = 'https://api.vk.com/method/users.get'
+    
+    url = 'https://api.vk.com/method/photos.get'
+    params = {'owner_id': user_id, 'access_token': token, 'album_id': 'profile', 'extended' : '1', 'photo_sizes': '1' , 'v': '5.131'}
 
     response = requests.get(url, params=params)
     data = response.json()
@@ -15,12 +16,15 @@ def get_user_data(token):
 
 if __name__ == '__main__':
         
-        
+    user_id = input("Enter user_id:")
+    ya_token = input("Enter Yandex.Disk's token:")
+
     with open("token.txt", 'r') as token_file:
         token = token_file.readline()
 
-    data = get_user_data(token)
-    pprint(data)
+    data = get_user_data(token, user_id, ya_token)
+    for element in data['response']['items']:
+        pprint(element['id'])
 
 
 
